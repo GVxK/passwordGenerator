266751a5e1b31f49c0b4769bb2f6eecc871db83a
GVxK <guilhermevxk@gmail.com>
2023-04-16 00:12:50 -0300

    3

---
+++ index.js
@@ -0,0 +1,21 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]; //[91]
+let password1 = []
+
+//Elements
+let password1El = document.getElementById("password1")
+let password2El = document.getElementById("password2")
+
+function getpassword() {
+    password1El.textContent = ""
+    for(let r = 0; r<characters.length;){
+    let passwordvar = Math.floor(Math.random()*91)
+    password1.push(characters[passwordvar]); r++
+    }    
+
+
+    for (let i = 0; i < password1.length; i++){
+        password1El.textContent += password1[i]
+    }
+  
+}
