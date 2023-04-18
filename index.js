e006a47c1bc7aa905378990bbb760077dc6116ad
GVxK <guilhermevxk@gmail.com>
2023-04-18 18:29:25 -0300

    2

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 
 //Variables
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"]; //[91]
-const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
-const numLet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-const numSymb = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
-const letSymb = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+let characters = []; //[91]
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+let symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+// const numLet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+// const numSymb = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+// const letSymb = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 let passwordLength = 20
 let passwordvar = ""
 let password1 = []
@@ -29,26 +28,41 @@ password2El.textContent = " "
 //Value
 
 //Functions
+function checks() {
+if (numbersCb.checked === true) {
+    characters.push(...numbers)
+  } else {
+
+  }
+
+}
+
+
 function getPassword() {
-  
+    let characters = []; 
+     numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+ letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+ symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+    // checks()
+    
     password1El.textContent = " "
     password2El.textContent = " "
     let password1 = []
     let password2 = [] 
-    
-
-    // if (inputVa == 1) {
-    //      passwordLength = 1
-    // } else if(inputVa === 2) {
-    //      passwordLength = 2
-    // } else {
-    //      passwordLength = 20
-    // }
-
-    
-     
-    if (numbersCb.checked === true && lettersCb.checked === true && symbolsCb.checked === true) {
-    for (let i = 0; i < passwordLength; i++){
+    if (numbersCb.checked === true) {
+        characters.push(...numbers)
+      } 
+    if (lettersCb.checked === true) {
+        characters.push(...letters)
+      } 
+    if (symbolsCb.checked === true) {
+        characters.push(...symbols)
+      }
+    if (inputVa.value > 20) {
+        inputVa.value = 20
+    }
+   
+    for (let i = 0; i < inputVa.value; i++){
         let passwordvar = Math.floor(Math.random()*characters.length)
         password1.push(characters[passwordvar])
         password1El.textContent += password1[i]
@@ -56,66 +70,9 @@ function getPassword() {
         password2.push(characters[passwordvar])
         password2El.textContent += password2[i]
     } 
-    } else if (numbersCb.checked === true && lettersCb.checked === false && symbolsCb.checked === false) {
-        for (let i = 0; i < passwordLength; i++){
-            let passwordvar = Math.floor(Math.random()*numbers.length)
-            password1.push(numbers[passwordvar])
-            password1El.textContent += password1[i]
-            passwordvar = Math.floor(Math.random()*numbers.length)
-            password2.push(numbers[passwordvar])
-            password2El.textContent += password2[i]
-        }  
-    } else if (numbersCb.checked === false && lettersCb.checked === true && symbolsCb.checked === false) {
-        for (let i = 0; i < passwordLength; i++){
-            let passwordvar = Math.floor(Math.random()*letters.length)
-            password1.push(letters[passwordvar])
-            password1El.textContent += password1[i]
-            passwordvar = Math.floor(Math.random()*letters.length)
-            password2.push(letters[passwordvar])
-            password2El.textContent += password2[i]
-        }  
-    } else if (numbersCb.checked === false && lettersCb.checked === false && symbolsCb.checked === true) {
-        for (let i = 0; i < passwordLength; i++){
-            let passwordvar = Math.floor(Math.random()*symbols.length)
-            password1.push(symbols[passwordvar])
-            password1El.textContent += password1[i]
-            passwordvar = Math.floor(Math.random()*symbols.length)
-            password2.push(symbols[passwordvar])
-            password2El.textContent += password2[i]
-        }  
-    } else if (numbersCb.checked === true && lettersCb.checked === true && symbolsCb.checked === false) {
-        for (let i = 0; i < passwordLength; i++){
-            let passwordvar = Math.floor(Math.random()*numLet.length)
-            password1.push(numLet[passwordvar])
-            password1El.textContent += password1[i]
-            passwordvar = Math.floor(Math.random()*numLet.length)
-            password2.push(numLet[passwordvar])
-            password2El.textContent += password2[i]
-        }  
-    } else if (numbersCb.checked === true && lettersCb.checked === false && symbolsCb.checked === true) {
-        for (let i = 0; i < passwordLength; i++){
-            let passwordvar = Math.floor(Math.random()*numSymb.length)
-            password1.push(numSymb[passwordvar])
-            password1El.textContent += password1[i]
-            passwordvar = Math.floor(Math.random()*numSymb.length)
-            password2.push(numSymb[passwordvar])
-            password2El.textContent += password2[i]
-        }  
-    } else if (numbersCb.checked === false && lettersCb.checked === true && symbolsCb.checked === true) {
-        for (let i = 0; i < passwordLength; i++){
-            let passwordvar = Math.floor(Math.random()*letSymb.length)
-            password1.push(letSymb[passwordvar])
-            password1El.textContent += password1[i]
-            passwordvar = Math.floor(Math.random()*letSymb.length)
-            password2.push(letSymb[passwordvar])
-            password2El.textContent += password2[i]
-        }  
-    }
-} 
+}
 
 function copyCb() {
     
-    navigator.clipboard.writeText("password1El").value
-    alert("Copied the text: " + password1El)
+    navigator.clipboard.writeText(password1El.textContent).value
 }
-
